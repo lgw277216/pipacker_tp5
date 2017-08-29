d35e26c534871eec609673aff92ca29935f9521c
lgw277216 <ligwhu@vip.qq.com>
2017-08-29 16:37:52 +0800

    倒数第二次提交

--- application/api/controller/Home.php
+++ application/api/controller/Home.php
@@ -21,6 +21,13 @@ class Home extends baseControll
         $pp_list = Db::table("pp_user")
                 ->where("user_id=$id")
                 ->select();
+        // print_r($pp_list);exit();
+        if (null == $pp_list[0]["user_bg"] || "" == $pp_list[0]["user_bg"]) {
+           $pp_list[0]["user_bg"] = "";
+        }
+        if (null == $pp_list[0]["user_photo"] || "" == $pp_list[0]["user_photo"]) {
+            $pp_list[0]["user_photo"] = "/public/static/pipacker/images/none-square.jpg";
+        } 
         if(!empty($pp_list)){                
             $this->reJson("0",$pp_list); 
         }else{
@@ -115,9 +122,22 @@ class Home extends baseControll
      * @param  int  $id
      * @return \think\Response
      */
-    public function delete($id)
+    public function delete()
     {
         //
+        $param = Request::instance()->param();
+        // print_r($param);exit();
+        if(!empty($param)){
+        // unset($param["comment_id"]);
+        $comment_id = $param["comment_id"];
+        if(Db::table("pp_message")->where("comment_id=$comment_id")->delete()){
+            $this->reJson("0",array(),"已删除评论");
+        }else{
+            $this->reJson("2",array(),"服务器处理失败");
+        }
+        }else{
+        $this->rejson("1",array(),"数据丢失了...");
+        }
     }
 
     public function savehead(Request $request)
@@ -138,6 +158,55 @@ class Home extends baseControll
         }else{
             $this->reJson("0",array(),"数据丢失了...");exit();
         }  
+    }
+
+    public function message()
+    {
+         $param = Request::instance()->param();
+        if(!empty($param)){
+            $comment_list = Db::table("pp_message")
+                    ->where($param)
+                    ->join("pp_user","pp_user.user_id = pp_message.user_id")
+                    ->field("pp_user.user_name,pp_user.user_phone,pp_user.user_photo,pp_user.user_id,pp_message.comment,pp_message.update_time,pp_message.works_id,pp_message.comment_id")
+                    ->order("pp_message.update_time desc")
+                    ->limit(10)
+                    ->select();
+            session_start();
+            if(!empty($_SESSION["user_info"])){
+                $user_id = $_SESSION["user_info"]["user_id"];
+                foreach($comment_list as $key => $val){
+                    if($user_id == $val["user_id"]){
+                        $comment_list[$key]["del_val"]=0;
+                    }else{
+                        $comment_list[$key]["del_val"]=1;
+                    }
+                }
+            }else{
+                foreach($comment_list as $key => $val){
+                    $comment_list[$key]["del_val"]=1;
+                }
+            }
+            $this->reJson("0",$comment_list);
+        }else{
+            $this->reJson("1",array(),"数据丢失了...");
+        }
+    }
+
+    public function save_message()
+    {
+        $param = Request::instance()->param();
+        // print_r($_FILES['works_src']);
+        if(!empty($param)){
+            // $param['works_para'] = json_encode($param['works_para']);
+            // $param['works_src'] = saveFile('pic_src');
+            $param['update_time'] = time();
+            Db::table("pp_message")->insert($param);
+            $redata["comment_id"] =  Db::table("pp_message")->getLastInsID();
+            $redata["update_time"] = $param['update_time'];
+            $this->reJson("0",$redata,"成功插入");
+        }else{
+            $this->reJson("2",array(),"数据丢失了...");
+        }
     }
 }
 
